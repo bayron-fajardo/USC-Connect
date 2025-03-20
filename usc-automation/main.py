d9bce81001b4b6155ed03cc95603f337a5a0ac74
Bayron-Fajardo <bayronfajardor@gmail.com>
2025-03-20 11:34:16 -0500

    Obtencion de tareas

--- usc-automation/main.py
+++ usc-automation/main.py
@@ -16,6 +16,7 @@ load_dotenv()
 user = os.getenv("User")
 password = os.getenv("Password")
 url = os.getenv("Link")
+url_calendario = os.getenv("Link_calendario")
 
 
 
@@ -55,3 +56,22 @@ for curso in cursos:
 
 for curso in cursos_info:
     print(f"📌 {curso['titulo']} - {curso['departamento']}")
+
+driver.get(url_calendario)
+
+tareas_lista = []
+tareas = driver.find_elements(By.CLASS_NAME, "row event_lists p0")
+
+for tarea in tareas:
+    try:
+        titulo = tarea.find_element(By.XPATH, "//*[@id=\"yui_3_18_1_1_1742487331958_24\"]/h3").text
+
+        detalles = tarea.find_element(By.XPATH, "//*[@id=\"yui_3_18_1_1_1742487331958_24\"]/p[2]")
+
+        fecha = tarea.find_element(By.XPATH, "//*[@id=\"yui_3_18_1_1_1742487331958_24\"]/ul/li[1]/a")
+
+        tareas_lista.append(f"Titulo: {titulo},\nDetalles: {detalles}\nFecha: {fecha}")
+    except Exception as e:
+        print(f"Error al obtener datos de una tarea: {e}")
+time.sleep(10)
+print(tareas_lista)
